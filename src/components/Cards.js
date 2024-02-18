f47b559dd1f0c5b162f78be4e2026711230a8194
Fatum Abdullahi <fatumabdullahi@Fatums-MacBook-Air.local>
2024-02-18 00:06:43 -0500

    added create/join group decision page

---
+++ src/components/Cards.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import { Image } from 'mui-image'
+import { CardActionArea } from '@mui/material';
+import Grid from '@mui/material/Grid';
+
+export default function ActionCard({ cardDetail, icon }) {
+    return (
+        <div>
+        <Card   sx={{ width: 500, maxWidth:600,  border: "1px solid #A9D6E5",  boxShadow: "1px 1px 1px 1px #A9D6E5"}}>
+            <CardActionArea  sx={{ minHeight:150}}>
+                <CardContent>
+
+                    <Grid item xs={12} display="flex" justifyContent="center" alignItems="center">
+                        <div>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {cardDetail}
+                            </Typography>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} display="flex" justifyContent="center" alignItems="center">
+                        <div>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            color="inherit"
+                        >
+                            <Image src={icon} width={30} sx={{ flexGrow: 1, justifyContent: 'center' }} />
+                        </IconButton>
+                        </div>
+                    </Grid>
+
+
+                </CardContent>
+            </CardActionArea>
+        </Card>
+        </div>
+    );
+}
--- src/pages/create/_tempindex.js
+++ src/pages/create/_tempindex.js
@@ -1,12 +1,24 @@
-import Navbar from "../../components/Navbar"
-import ActionCard from "../../components/Cards";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import ActionCard from '../../components/Cards'
+// const inter = Inter({ subsets: ["latin"] });
+// import secureLocalStorage from "react-secure-storage";
+import Navbar from "../../components/Navbar";
+import { useRouter } from 'next/navigation'
 
 export default function Create() {
+    const router = useRouter()
+
+    // console.log(secureLocalStorage.getItem("key-name"))
+    // console.log(secureLocalStorage.getItem("number"))
     return (
-        <>
+        <div sx={{ bgcolor: '#fff' }}>
             <Navbar />
+
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <div sx={{}} >
@@ -15,6 +27,7 @@ export default function Create() {
                         </Typography>
                     </div>
                 </Grid>
+
                 <Grid item xs={6} display="flex" justifyContent="center" alignItems="center" sx={{pb:2}}>
                 
                         <ActionCard cardDetail={'Create a group'} icon="createGroupIcon.png"/>
@@ -22,10 +35,13 @@ export default function Create() {
                 </Grid>
                 <Grid item xs={6} display="flex" justifyContent="center" alignItems="center" sx={{pb:2}}>
                 
-                        <ActionCard cardDetail={'Join a group'} icon="joinGroupIcon.png"/>
-                    
+                      <ActionCard cardDetail={'Join a group'} icon="joinGroupIcon.png"/>
+                 
                 </Grid>
-            </Grid>
-        </>
+                
+            </Grid> 
+
+
+        </div>
     );
 }
